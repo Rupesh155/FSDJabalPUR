9a105958449eb4f96d88e49b986ec5eafcc5b733
Rupesh155 <rudhg35@gmail.com>
2025-01-13 12:11:02 +0530

    js

---
+++ lect16/index.js
@@ -0,0 +1,85 @@
+
+// Number
+// a=5.5
+// String  'c' 'cc' "ccc" 
+// Boolean true ,false 0, 1 
+// null
+// undefined
+// Symbol
+// BigInt
+// int a
+
+// var ,let ,const
+// var a  = 5
+// console.log(a);
+// var a=5
+// var a=10
+// console.log(a);
+// let a=5
+//   a
+// console.log(a);
+
+// const c=100
+//  c=3000
+// console.log(c);
+
+// console.log(0.1+0.2==0.3);
+
+// var a=100
+// var to =a==100
+// console.log(to);
+
+// let a='rahul'
+// let b='jain'
+// console.log(`${a}       ${b}`);
+
+// = == ===
+
+// console.log();
+
+// console.log(false=='false');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
